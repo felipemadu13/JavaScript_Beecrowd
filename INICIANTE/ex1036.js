7b3ec3b554e4d1da4aabd447f5913c6605a7da85
Felipe Madureira <106445418+felipemadu13@users.noreply.github.com>
2022-08-17 21:12:46 -0300

    ex1035

---
+++ INICIANTE/ex1036.js
@@ -0,0 +1,10 @@
+let input = require('fs').readFileSync('stdin', 'utf8');
+let lines = input.split(' ');
+
+var a = lines.shift();
+var b = lines.shift();
+var c = lines.shift();
+
+console.log(a);
+console.log(b);
+console.log(c);
