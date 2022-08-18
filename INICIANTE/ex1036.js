22d5a789647ece8f1df033681674368e6a9ab965
Felipe Madureira <106445418+felipemadu13@users.noreply.github.com>
2022-08-18 08:51:20 -0300

    ex1036

--- INICIANTE/ex1036.js
+++ INICIANTE/ex1036.js
@@ -1,10 +1,23 @@
-let input = require('fs').readFileSync('stdin', 'utf8');
+let input = require('fs').readFileSync('/dev/stdin', 'utf8');
 let lines = input.split(' ');
 
-var a = lines.shift();
-var b = lines.shift();
-var c = lines.shift();
+let n1 = parseFloat(lines.shift());
+let n2 = parseFloat(lines.shift());
+let n3 = parseFloat(lines.shift());
 
-console.log(a);
-console.log(b);
-console.log(c);
+let a = n1.toFixed(3);
+let b = n2.toFixed(3);
+let c = n3.toFixed(3);
+
+let delta = b**2 - 4 * a * c
+
+if (delta < 0 || a == 0) {
+    console.log(`Impossivel calcular`)
+} else{
+    let bhaskara1 = (-b + Math.sqrt(delta)) /  (2 * a)
+    let bhaskara2 = (-b - Math.sqrt(delta)) / (2 * a)
+
+console.log(`R1 = ${bhaskara1.toFixed(5)}`);
+console.log(`R2 = ${bhaskara2.toFixed(5)}`);
+}
+
