6e9fe42a7a09175b9e9256106c505826bfb727ed
Felipe Madureira <106445418+felipemadu13@users.noreply.github.com>
2022-08-18 09:30:40 -0300

    Estruturas e Bibliotecas

---
+++ ESTRUTURAS_E_BIBLIOTECAS/entrada.js
@@ -0,0 +1,8 @@
+//entrada - testes
+let input = require('fs').readFileSync('stdin', 'utf8');
+let lines = input.split('\n');
+
+//entrada - beecrowd
+let input = require('fs').readFileSync('/dev/stdin', 'utf8');
+let lines = input.split('\n');
+
---
+++ ESTRUTURAS_E_BIBLIOTECAS/ex1022.js
@@ -0,0 +1,6 @@
+let input = require('fs').readFileSync('stdin', 'utf8');
+let lines = input.split('\n');
+
+let a = lines.shift();
+
+console.log(a)
